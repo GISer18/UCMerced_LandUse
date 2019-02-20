db4f2bef5128241d9acb5b292b313339c782eeb2
icedumpy <45779317+icedumpy@users.noreply.github.com>
2019-02-20 10:49:43 +0700

    Add files via upload

--- Classification.py
+++ Classification.py
@@ -1,26 +1,27 @@
 import keras
-from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
 from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, AveragePooling2D, Flatten
+from keras.layers import Dense, AveragePooling2D, Flatten, Conv2D
 from keras import backend as K
 import matplotlib.pyplot as plt
 import h5py
 from keras.callbacks import ModelCheckpoint,ReduceLROnPlateau,TensorBoard
-from sklearn.model_selection import train_test_split
+from sklearn.utils import class_weight
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np 
 #%%
 print(K.image_data_format())
-model_name = 'log2'
-data_name = 'traintest'
+model_name = 'logv.2_1'
+data_name = 'traintest2'
 filepath='weights/%s.hdf5'%(model_name)
 
-tensorboard = TensorBoard(log_dir='./logs/1', histogram_freq=0,batch_size=128)  
+tensorboard = TensorBoard(log_dir='./logs/v.2_1', histogram_freq=0,batch_size=128)  
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-5)
-checkpoint = ModelCheckpoint('weights/%s.{epoch:02d}-{val_loss:.2f}.hdf5'%(model_name), monitor='val_acc', verbose=0, save_best_only=True, mode='max')
+checkpoint = ModelCheckpoint('weights/%s.{epoch:02d}-{val_loss:.2f}.hdf5'%(model_name), monitor='val_loss', verbose=0, save_best_only=True, mode='min')
 callbacks_list = [checkpoint,tensorboard]#,reduce_lr]
 #%%
-batch_size = 1
+batch_size = 8
 num_classes = 17
-epochs = 500
+epochs = 100
 img_size = 256
 input_shape = (img_size, img_size, 3)
 #%% load test
@@ -34,33 +35,94 @@ with h5py.File('dataset/%s.h5'%(data_name), 'r') as f:
     y_train = f['y_train'][()]
     y_test = f['y_test'][()]
 #%%
-x_train = preprocess_input(x_train)
-x_test = preprocess_input(x_test)
-#%%
-base_model = InceptionResNetV2(weights=None, include_top=False,input_shape=(256,256,3))
-x = base_model.output
-x = AveragePooling2D((6,6))(x)
+y_ints = [y.argmax() for y in y_train]
+class_weights = class_weight.compute_class_weight('balanced',
+                                                 np.unique(y_ints),
+                                                 y_ints)
+#%% simple model
+from keras.applications.vgg16 import VGG16
+base_model = VGG16(weights=None, include_top=False,input_shape=(256,256,3))
+base_model.summary()
+#%%    
+x = base_model.get_layer('block3_conv3').output
+x = Conv2D(256,kernel_size=(3,3),padding='same')(x)
+x = keras.layers.Activation("relu")(x)
+x = AveragePooling2D((64,64))(x)
 x = Flatten()(x)
 predictions = Dense(17, activation='sigmoid')(x)
-model = Model(inputs=base_model.input, outputs=predictions)    
-#%%    
-optimizer = keras.optimizers.Adadelta()
-loss = 'binary_crossentropy'
-model.compile(optimizer= optimizer,
-              loss = loss, 
-              metrics = ['accuracy']        
-             )
-model.summary()
+model = Model(inputs=base_model.input, outputs=predictions) 
 # In[10]:
-history = model.fit(x_train, y_train, 
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data = (x_test, y_test),
-          callbacks=callbacks_list,
-          verbose=1
-         )
-#%% save and load part
-#model_json = model.to_json()
-name = '%s_last'%(model_name)
-## save model and weight
-model.save('weights/%s.hdf5'%(name))
+optimizer = keras.optimizers.Adadelta()
+model.compile(optimizer= optimizer,loss='binary_crossentropy',metrics=['accuracy'])
+model.summary()
+
+datagen = ImageDataGenerator(
+        rescale=1./255,
+        horizontal_flip=True,
+        vertical_flip=True,
+)
+x_test = (x_test/255.0).astype(np.float32)
+#%%
+history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                              validation_data = (x_test,y_test),
+                              steps_per_epoch=len(x_train) // batch_size,
+                              class_weight=class_weights,
+                              epochs=200,
+                              verbose=1,
+                              callbacks = callbacks_list
+                              )
+import pickle
+with open('weights/%s.pickle'%(model_name), 'wb') as file_pi:
+    pickle.dump(history.history, file_pi) 
+#%%    
+#%%
+preds = model.predict(x_test)
+preds[preds>=0.5] = 1
+preds[preds<0.5] = 0
+#%%
+import matplotlib
+matplotlib.style.use('seaborn')
+epochs = len(history.history['loss'])
+max_loss = max(max(history.history['loss']), max(history.history['val_loss']))
+plt.axis([0, epochs+1, 0, round(max_loss * 2.0) / 2 + 0.5])
+x = np.arange(1, epochs+1)
+plt.plot(x, history.history['loss'])
+plt.plot(x, history.history['val_loss'])
+plt.title('Training loss vs. Validation loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Training', 'Validation'], loc='right')
+plt.show()
+#%%
+epochs = np.argmin(history.history['val_loss']) + 1
+print(f'Stop training at {epochs} epochs')
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Datamaker.py
+++ Datamaker.py
@@ -5,8 +5,32 @@ import pandas as pd
 import os
 import h5py
 import cv2
+from sklearn.model_selection import train_test_split
+import time
+from skimage.io import imread, imsave
+import warnings
 #%%
-def generate_dataset_rough(data_dir,label_dir):
+def dataset_1():
+    x,y,groundtruth = generate_dataset_rough(data_dir,label_dir,groundtruth_dir)
+    hf = h5py.File('dataset/data.h5', 'w')
+    hf.create_dataset('img',data=x)
+    hf.create_dataset('label',data=y)
+    hf.create_dataset('groundtruth',data=groundtruth)
+    hf.close()
+    x_train, x_test, y_train, y_test, groundtruth_train, groundtruth_test  = train_test_split(x, y,groundtruth, test_size=0.2, random_state=1)
+    x_train, x_val, y_train, y_val, groundtruth_train, groundtruth_val  = train_test_split(x_train, y_train, groundtruth_train, test_size=0.2, random_state=1)    
+    hf = h5py.File('dataset/traintest.h5', 'w')
+    hf.create_dataset('x_train',data=x_train)
+    hf.create_dataset('x_test',data=x_test)
+    hf.create_dataset('x_val',data=x_val)
+    hf.create_dataset('y_train',data=y_train)
+    hf.create_dataset('y_test',data=y_test)
+    hf.create_dataset('y_val',data=y_val)
+    hf.create_dataset('groundtruth_train',data=groundtruth_train)
+    hf.create_dataset('groundtruth_test',data=groundtruth_test)
+    hf.create_dataset('groundtruth_val',data=groundtruth_val)
+    hf.close()
+def generate_dataset_rough(data_dir,label_dir,groundtruth_dir):
     #For label
     df = pd.read_excel(label_dir)
     label_df = df[class_names].copy()
@@ -20,18 +44,34 @@ def generate_dataset_rough(data_dir,label_dir):
         print(folder)
         image_dir = [folder + '\\' +item for item in os.listdir(folder)]
         for image_name in image_dir:
-            image = gdal.Open(image_name)
-            image = image.ReadAsArray()
-            image = np.moveaxis(image,0,-1)
+            image=imread(image_name)
+#            image = gdal.Open(image_name)
+#            image = image.ReadAsArray()
+#            image = np.moveaxis(image,0,-1)
             image = cv2.resize(image,(256,256),interpolation=cv2.INTER_CUBIC)
-#            image = image/255.0
             x[idx] = image
             idx+=1
-    return x,y
+    idx = 0
+    groundtruth_sub_dir = [groundtruth_dir + '\\' +item for item in os.listdir(groundtruth_dir)]
+    groundtruth = np.zeros((y.shape[0],256,256,3),dtype=np.float32)
+    for folder in groundtruth_sub_dir:
+        print(folder)
+        groundtruth_label_dir = [folder + '\\' +item for item in os.listdir(folder)]
+        for groundtruth_name in groundtruth_label_dir:
+            image = cv2.imread(groundtruth_name)
+            image = image[...,::-1]
+            groundtruth[idx] = image
+            idx+=1
+    return x.astype(np.int16),y,groundtruth.astype(np.int16)
+
+def extract_color(groundtruth,rgb_value):
+    color = np.equal(groundtruth,rgb_value)
+    color = np.logical_and(np.logical_and(color[:,:,0],color[:,:,1]),color[:,:,2]).astype(np.uint16) *1 
+    return color
 #%%
 data_dir = r"D:\Thesis\Work2\UCMerced_LandUse\Images"
 label_dir = r"D:\Thesis\Work2\DLRSD\multi-labels.xlsx"
-ground_truth_dir = r"D:\Thesis\Work2\DLRSD\Images"
+groundtruth_dir = r"D:\Thesis\Work2\DLRSD\Images"
 class_names = ['airplane', 'baresoil', 'buildings', 'cars', 'chaparral',
                'court', 'dock', 'field', 'grass', 'mobilehome', 'pavement', 'sand',
                'sea', 'ship', 'tanks', 'trees', 'water'] 
@@ -53,23 +93,105 @@ class_rgb_value = np.array([[166,202,240],
                             [0,255,0],
                             [0,255,255]
                             ])
+#%% 
+if __name__ == "__main__":
+    x,y,groundtruth = generate_dataset_rough(data_dir,label_dir,groundtruth_dir) 
+#%% create sub directory for each class
+    data_type = ['input','groundtruth']
+    if not os.path.isdir('data'):
+        os.mkdir('data')
+        for data in data_type:
+            os.mkdir(os.path.join('data',data))
+            for class_name in class_names:
+                class_subdir = os.path.join('data',data,class_name)
+                os.mkdir(class_subdir)
 #%%
-x,y = generate_dataset_rough(data_dir,label_dir)
+    warnings.simplefilter('ignore', UserWarning)
+    class_count = np.zeros((1,17),dtype = np.uint16)
+    for i in range(x.shape[0]):
+        groundtruth_class = np.where(y[i]==1)[0]
+        for idx in groundtruth_class:
+            color_mask = np.expand_dims(extract_color(groundtruth[i],class_rgb_value[idx]),axis=2)
+            masked = (x[i,:,:] * color_mask).astype(np.int16)
+            save_input_dir  = os.path.join('data',data_type[0],class_names[idx],class_names[idx]+'%04d.png'%(class_count[:,idx]))
+            save_ground_dir  = os.path.join('data',data_type[1],class_names[idx],class_names[idx]+'%04d.png'%(class_count[:,idx]))
+            color_mask = np.squeeze(color_mask)
+            imsave(save_input_dir,masked)
+            imsave(save_ground_dir,color_mask*255)
+            class_count[:,idx] +=1
+        if i%100==0:
+            print(i)
+#            plt.figure(1)
+#            plt.imshow(x[i])
+#            plt.figure(2)
+#            plt.imshow(masked)
+#            plt.show()
 #%%
-hf = h5py.File('dataset/data_notnormalize.h5', 'w')
-hf.create_dataset('img',data=x)
-hf.create_dataset('label',data=y)
-hf.close()
+    source_dir = os.path.join('data', 'input')
+    flow_base = os.path.join('data', 'flow')
+    count1 = 0
+    count2 = 0
+    x = np.zeros((class_count.sum(),256,256,3),dtype=np.int16)
+    y = np.zeros((class_count.sum(),17),dtype=np.int8)
+    class_names = os.listdir(source_dir)    
+    for class_name in class_names:
+        class_subdir = os.path.join(source_dir,class_name)
+        print(class_subdir)
+        for root, _, filenames in os.walk(class_subdir):
+            for filename in filenames:
+                target_name = os.path.join(class_subdir,filename)
+                x[count1] = imread(target_name)
+                y[count1,count2] = 1
+                count1+=1
+        count2+=1
+    
+    source_dir = os.path.join('data', 'groundtruth')
+    flow_base = os.path.join('data', 'flow')
+    count1 = 0
+    groundtruth = np.zeros((class_count.sum(),256,256),dtype=np.int16)
+    class_names = os.listdir(source_dir)    
+    for class_name in class_names:
+        class_subdir = os.path.join(source_dir,class_name)
+        print(class_subdir)
+        for root, _, filenames in os.walk(class_subdir):
+            for filename in filenames:
+                target_name = os.path.join(class_subdir,filename)
+                groundtruth[count1] = imread(target_name)
+                count1+=1
+        count2+=1
+#%%     
+    hf = h5py.File('dataset/data2.h5', 'w')
+    hf.create_dataset('img',data=x)
+    hf.create_dataset('label',data=y)
+    hf.create_dataset('groundtruth',data=groundtruth)
+    hf.close()
+    x_train, x_test, y_train, y_test, groundtruth_train,groundtruth_test = train_test_split(x, y,groundtruth,
+                                                        test_size=0.2,
+                                                        random_state=42,
+                                                        stratify=y)
+    del x 
+    del y
+    del groundtruth
 #%%
-from sklearn.model_selection import train_test_split
-x_train,x_test, y_train, y_test = train_test_split(x, y, test_size=0.5 , shuffle=True) 
-#%%
-hf = h5py.File('dataset/traintest.h5', 'w')
-hf.create_dataset('x_train',data=x_train)
-hf.create_dataset('x_test',data=x_test)
-hf.create_dataset('y_train',data=y_train)
-hf.create_dataset('y_test',data=y_test)
-hf.close()
+    hf = h5py.File('dataset/traintest2.h5', 'w')
+    hf.create_dataset('x_train',data=x_train)
+    hf.create_dataset('x_test',data=x_test)
+    hf.create_dataset('y_train',data=y_train)
+    hf.create_dataset('y_test',data=y_test)
+    hf.create_dataset('groundtruth_train',data=groundtruth_train)
+    hf.create_dataset('groundtruth_test',data=groundtruth_test)
+    hf.close()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
