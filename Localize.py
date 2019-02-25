39dc976d2f14deb23f8c09527f406295d9e82734
icedumpy <45779317+icedumpy@users.noreply.github.com>
2019-02-25 21:39:05 +0700

    Add files via upload

--- Localize.py
+++ Localize.py
@@ -13,7 +13,9 @@ from keras.applications.inception_resnet_v2 import preprocess_input
 import cv2
 import pickle
 import time
+import itertools
 
+from sklearn.metrics import confusion_matrix
 def get_confision_matrix(y,preds):
     confusion_matrix = np.zeros((preds.shape[1],2,2))
     for pred_idx in range(len(y)):
@@ -37,13 +39,17 @@ def get_confision_matrix(y,preds):
     FA = np.zeros((1,y.shape[1]))
     for idx in range(confusion_matrix.shape[0]):
         FA[:,idx] = (confusion_matrix[idx,0,1])/ (confusion_matrix[idx,0,0]+confusion_matrix[idx,0,1])
+    Precision =  np.zeros((1,y.shape[1]))  
+    for idx in range(confusion_matrix.shape[0]):
+        Precision[:,idx] = (confusion_matrix[idx,1,1])/ (confusion_matrix[idx,0,1]+confusion_matrix[idx,1,1]) 
+        
     Kappa = np.zeros((1,y.shape[1]))
     for idx in range(confusion_matrix.shape[0]):
         p_0 = ((confusion_matrix[idx,0,0] + confusion_matrix[idx,0,1])/confusion_matrix[idx].sum()) * ((confusion_matrix[idx,0,0] + confusion_matrix[idx,1,0])/confusion_matrix[idx].sum())
         p_1 = ((confusion_matrix[idx,1,0] + confusion_matrix[idx,1,1])/confusion_matrix[idx].sum()) * ((confusion_matrix[idx,1,1] + confusion_matrix[idx,0,1])/confusion_matrix[idx].sum())
         p_e = p_0 + p_1
         Kappa[:,idx] = (OA[:,idx]-p_e)/(1-p_e) 
-    return confusion_matrix , OA , DP , FA, Kappa
+    return confusion_matrix , OA , DP , FA, Precision, Kappa
   
 def extract_color(groundtruth,rgb_value):
     color = np.equal(groundtruth,rgb_value)
@@ -98,50 +104,33 @@ def generate_heatmap(x,model):
     heatmap[i] = layer_output
     print(i,'/',x.shape[0])
   return heatmap
-#%%
-channel_means = np.load('mean.npy')
-channel_stds = np.load('std.npy')
-model_name = 'log14.142-0.24'
-data_name = 'traintest'
-hf = h5py.File('dataset/%s.h5'%(data_name), 'r')
-print(list(hf.keys()))
-hf.close()
-#%% download data from test set
-with h5py.File('dataset/%s.h5'%(data_name), 'r') as f:
-    x_test  = f['x_val'][()]
-    y_test  = f['y_val'][()]
-    groundtruth_test = f['groundtruth_val'][()]
-x_test = x_test.astype(np.float32)
-x_test = (x_test-channel_means)/channel_stds
-x_test = (x_test-x_test.min())/(x_test.max()-x_test.min())
-x_test = x_test.astype(np.float32)
-#%%
-with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
-    model = load_model('weights/%s.hdf5'%(model_name) , custom_objects={"weighted_loss": 'binary_crossentropy'})
-#with open('weights/%s.pickle'%(model_name), 'rb') as input_file:
-#    e = pickle.load(input_file)
-model.summary()
-#%%
-img_size = 256
-CNN_reso = 64
-CNN_channel = 256
-ratio = img_size/CNN_reso # ratio to change resolution of output of last CNN channel
 
-dense_weight = model.layers[-1].get_weights()[0]
-
-preds = model.predict(x_test,batch_size=16)
-
-preds[preds>=0.5] = 1
-preds[preds<0.5] = 0
-preds = preds.astype(np.int16)
-
-confusion_matrix,OA,DP,FA, Kappa = get_confision_matrix(y_test,preds)
-print('Classification performance\n--------------------------')
-print('OA:    ',OA.mean())
-print('DP:    ',DP.mean())
-print('FA:    ',FA.mean())
-print('Kappa: ',Kappa.mean())
-
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    
 class_names = ['airplane', 'baresoil', 'buildings', 'cars', 'chaparral',
                'court', 'dock', 'field', 'grass', 'mobilehome', 'pavement', 'sand',
                'sea', 'ship', 'tanks', 'trees', 'water'] 
@@ -164,23 +153,98 @@ class_rgb_value = np.array([[166,202,240],
                             [0,255,255]
                             ])
 #%%
-i = 3
-preds_class = np.where(preds[i]==1)
-print(y_test[i])
-print(preds[i])
-print(preds_class[0])
-print('Predict =',[class_names[idx] for idx in preds_class[0]])
-plt.imshow(x_test[i])
+multilabel = True
+model_name = 'logv.2_2-post.45-0.21'
+if multilabel:
+    data_name = 'traintest'
+else:
+    data_name = 'traintest2'
+hf = h5py.File('dataset/%s.h5'%(data_name), 'r')
+print(list(hf.keys()))
+hf.close()
+#%% download data from test set
+with h5py.File('dataset/%s.h5'%(data_name), 'r') as f:
+    x_test  = f['x_test'][()]
+    y_test  = f['y_test'][()]
+    groundtruth_test = f['groundtruth_test'][()]
+x_test = (x_test/255.0).astype(np.float32)
 #%%
-confusion_matrix_localize = np.zeros((y_test.shape[1],2,2))
-#plt.figure(1)
-#plt.imshow(x_test[i])
-for i in range(len(y_test)):
-    preds_class = np.where(preds[i]==1)
-    print(i,'/',len(y_test))
+with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+    model = load_model('weights/%s.hdf5'%(model_name) , custom_objects={"weighted_loss": 'binary_crossentropy'})
+#with open('weights/%s.pickle'%(model_name), 'rb') as input_file:
+#    e = pickle.load(input_file)
+model.summary()
+#%%
+#score = model.evaluate(x_test, y_test,batch_size=16)
+#print('Loss =',score[0])
+#print('Acc =',score[1])
+#%%
+img_size = 256
+CNN_reso = 64
+CNN_channel = 512
+ratio = img_size/CNN_reso # ratio to change resolution of output of last CNN channel
+dense_weight = model.layers[-1].get_weights()[0]
+#%%
+y_pred = model.predict(x_test,batch_size=16)
+theta = 0.6
+#%%
+if multilabel:
+    y_pred[y_pred>=theta] = 1
+    y_pred[y_pred<theta] = 0
+    y_pred = y_pred.astype(np.int16)
+    cnf_matrix,OA,DP,FA,Precision, Kappa = get_confision_matrix(y_test,y_pred)
+    print('Classification performance\n--------------------------')
+    print('OA:        ',OA.mean())
+    print('Precision: ',Precision.mean())
+    print('DP,Recall: ',DP.mean())
+    print('FA:        ',FA.mean())
+    F_score = (2*Precision.mean()*DP.mean())/(Precision.mean()+DP.mean())
+    print('F-score:   ',F_score)
+    print('Kappa:     ',Kappa.mean())
+else:
+    y_pred = np.argmax(y_pred,axis=1)
+    y_pred = y_pred.reshape(-1,1)
+    y_true = np.array([y.argmax() for y in y_test]).reshape(-1,1)
+    cnf_matrix = confusion_matrix(y_true, y_pred)
+    np.set_printoptions(precision=2)
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names,
+                          title='Confusion matrix, without normalization')
+    from sklearn.metrics import cohen_kappa_score
+    print('kappa:',cohen_kappa_score(y_true,y_pred))
+    from sklearn.metrics import precision_score
+    print('precision:',precision_score(y_true, y_pred, average='macro')) 
+    from sklearn.metrics import recall_score
+    print('recall',recall_score(y_true, y_pred, average='macro')) 
+    class_count = np.zeros((1,17),dtype = np.uint16)
+    for i in range(y_true.shape[0]):
+        class_count[:,y_true[i]] += 1
+#%%
+if multilabel:
+    i = 0
+    preds_class = np.where(y_pred[i]==1)
+    print(y_test[i])
+    print(y_pred[i])
+    print(preds_class[0])
+    print('Predict =',[class_names[idx] for idx in preds_class[0]])
+    plt.imshow(x_test[i])      
+else:
+    i = 0
+    print(y_true[i])
+    print(y_pred[i])
+    plt.imshow(x_test[i])      
+#%%
+i = 0
+plt.imshow(x_test[i])
+if multilabel:
+    preds_class = np.where(y_pred[i]==1)
+    print(y_test[i])
+    print(y_pred[i])
+    print('Predict =',[class_names[idx] for idx in preds_class[0]])
+    preds_class = np.where(y_pred[i]==1)
     for idx,class_idx in enumerate(preds_class[0]):
-    #    plt.figure(idx+2)
-    #    plt.xlabel(class_names[class_idx])
+        plt.figure(idx+2)
+        plt.xlabel(class_names[class_idx])
         weight_GAP = dense_weight[:,class_idx] #weight of flatten channel
         get_last_conv_output = K.function([model.layers[0].input],
                                       [model.layers[-4].output])  #must be output from last CNN channel 
@@ -188,7 +252,7 @@ for i in range(len(y_test)):
         layer_output = np.squeeze(layer_output) # change from 1xNxNxCNNchannel to NxNxCNNchannel
         layer_output = scipy.ndimage.zoom(layer_output, (ratio, ratio, 1), order=1) # scale the output of last CNN channel by raio dim: 112 x 112 x 128
         CAM = np.dot(layer_output.reshape((img_size*img_size, CNN_channel)), weight_GAP).reshape(img_size,img_size) # get Class Activation Map 
-    #    plt.imshow(CAM,cmap='jet')
+        plt.imshow(CAM,cmap='jet')
        #background/foreground segment by graphcut
         CAM = 255 * (CAM-CAM.min())/(CAM.max() - CAM.min()) #normalize CAM to [0,255]
         g = maxflow.Graph[int]()
@@ -201,68 +265,29 @@ for i in range(len(y_test)):
     #    plt.imshow(sgm,cmap='gray',alpha=1) #white is foreground
         location = np.where(sgm==1)
         mask = np.zeros((sgm.shape[0],sgm.shape[1],3),dtype=np.int16)
-        mask[location[0],location[1],:] = class_rgb_value[class_idx]
-    #    plt.imshow(mask)   
-        for x in range(256):
-            for y in range(256):
-                if(  (groundtruth_test[i][x,y,:] == class_rgb_value[class_idx]).all()  ):
-                    if(  (mask[x,y,:] == class_rgb_value[class_idx]).all()  ):
-                        confusion_matrix_localize[class_idx,1,1] += 1
-                    else:
-                        confusion_matrix_localize[class_idx,1,0] += 1
-                else:
-                    if(  (mask[x,y,:] == class_rgb_value[class_idx]).all()  ):
-                        confusion_matrix_localize[class_idx,0,1] += 1
-                    else:
-                        confusion_matrix_localize[class_idx,1,0] += 1
-#plt.figure(idx+3)
-#plt.xlabel('GroundTruth')
-#plt.imshow(groundtruth_test[i])
-np.save('log11.npy',confusion_matrix_localize)        
-#%%
-print(confusion_matrix_localize)
-#%%
-i = 0
-preds_class = np.where(preds[i]==1)
-print(preds[i])
-print(y_test[i])
-print('Predict =',[class_names[idx] for idx in preds_class[0]])
-plt.imshow(x_test[i])
-preds_class = np.where(preds[i]==1)
-for idx,class_idx in enumerate(preds_class[0]):
-    plt.figure(idx+2)
-    plt.xlabel(class_names[class_idx])
-    weight_GAP = dense_weight[:,class_idx] #weight of flatten channel
+        mask[location[0],location[1],:] = class_rgb_value[class_idx]      
+        plt.imshow(mask)
+    plt.figure(idx+3)
+    plt.imshow(groundtruth_test[i])
+else:
+    print(y_true[i])
+    print(y_pred[i])
+    weight_GAP = dense_weight[:,y_pred[i]] #weight of flatten channel
     get_last_conv_output = K.function([model.layers[0].input],
                                   [model.layers[-4].output])  #must be output from last CNN channel 
     layer_output = get_last_conv_output([x_test[i].reshape((1,img_size,img_size,3))])[0] #get output of last CNN channel
     layer_output = np.squeeze(layer_output) # change from 1xNxNxCNNchannel to NxNxCNNchannel
     layer_output = scipy.ndimage.zoom(layer_output, (ratio, ratio, 1), order=1) # scale the output of last CNN channel by raio dim: 112 x 112 x 128
     CAM = np.dot(layer_output.reshape((img_size*img_size, CNN_channel)), weight_GAP).reshape(img_size,img_size) # get Class Activation Map 
+    plt.figure(2)
     plt.imshow(CAM,cmap='jet')
-   #background/foreground segment by graphcut
-    CAM = 255 * (CAM-CAM.min())/(CAM.max() - CAM.min()) #normalize CAM to [0,255]
-    g = maxflow.Graph[int]()
-    nodeids = g.add_grid_nodes(CAM.shape)
-    g.add_grid_edges(nodeids, 1) 
-    g.add_grid_tedges(nodeids, CAM, 255-CAM) 
-    g.maxflow()
-    sgm = g.get_grid_segments(nodeids)
-    sgm = np.int_(np.logical_not(sgm))
-#    plt.imshow(sgm,cmap='gray',alpha=1) #white is foreground
-    location = np.where(sgm==1)
-    mask = np.zeros((sgm.shape[0],sgm.shape[1],3),dtype=np.int16)
-    mask[location[0],location[1],:] = class_rgb_value[class_idx]      
-    plt.imshow(mask)
-plt.figure(idx+3)
-plt.imshow(groundtruth_test[i])
 #%% generate_heatmap_pixel
 heatmap = generate_heatmap(x_test,model)
 groundtruth_ratio = generate_groundtrurh_ratio(groundtruth_test,CNN_reso)
 np.save('heatmap.npy',heatmap)
 np.save('groundtruth_class_ratio.npy',groundtruth_ratio)
 
-
+#%%
 
 
 
